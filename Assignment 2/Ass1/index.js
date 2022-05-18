92b5e55368a554a183bd3b1e9d28adfe40c943b5
Shubham Somwanshi <69418944+shu3102@users.noreply.github.com>
2022-05-18 08:06:58 +0530

    Add files via upload
    1st try on the second assignment

---
+++ Assignment 2/Ass1/index.js
@@ -0,0 +1,38 @@
+
+var slideIndex = 0;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += 1);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex -= 1);
+}
+
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 0}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+
+
+
